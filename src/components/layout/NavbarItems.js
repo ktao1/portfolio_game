05ac2a456e484650824d9da00fb4b1e17043416d
meowkun <ktao1@umbc.edu>
2021-05-09 14:11:59 -0400

    test

--- src/components/layout/NavbarItems.js
+++ src/components/layout/NavbarItems.js
@@ -1,22 +1,22 @@
 export const NavbarItems = [
   {
     title: "Home",
-    url: "/",
+    url: "/portfolio",
     cName: "nav-links",
   },
   {
     title: "About",
-    url: "/About",
+    url: "/portfolio/About",
     cName: "nav-links",
   },
   {
     title: "Project",
-    url: "/Project",
+    url: "/portfolio/Project",
     cName: "nav-links",
   },
   {
     title: "Contact Me",
-    url: "/ContactMe",
+    url: "/portfolio/ContactMe",
     cName: "nav-links",
   },
 ];
